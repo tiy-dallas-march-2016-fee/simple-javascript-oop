0cde1a05a9cc183fd33989d13be240d5d28355d0
Jacolby Green <darknightnov@gmail.com>
2016-05-17 09:21:14 -0500

    Paragraph added to body

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 'use strict';
 
-function Car() {
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+  this.passengers = [];
+}
+
+Car.prototype = {
+  honk: function() {
+    return "beep beep";
+  },
+  addPassenger: function(passenger) {
+    this.passengers.push(passenger);
+  },
+  description: function() {
+    return "This is a " +  this.make + " " + this.model;
+  },
+  passengerList: function() {
+    if (this.passengers.length === 0) {
+      return "no passengers";
+    }
+    else if (this.passengers.length === 1) {
+      return this.passengers[0];
+    }
+    else {
+    this.passenger.forEach(function(passenger){
+      console.log(passenger);
+    });
+   }
+  }
 }
